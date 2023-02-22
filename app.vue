d82c8e10fe130cb4462828db9a7a08b680555c90
MBrian Website <muhammad.brian.27@gmail.com>
2023-02-22 18:54:42 +0700

    SET UP BASE ASSETS

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <div class="font-mono font-extrabold text-2xl text-green-700 text-center w-full mt-16">Installed TailwindCSS</div>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>

877abb9e15cb3bc53745fc48ad60aef9de579cd4
MBrian Website <muhammad.brian.27@gmail.com>
2023-02-22 17:08:51 +0700

    INSTALL TAILWINDCSS

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <div class="font-mono font-extrabold text-2xl text-green-700 text-center w-full mt-16">Installed TailwindCSS</div>
   </div>
 </template>

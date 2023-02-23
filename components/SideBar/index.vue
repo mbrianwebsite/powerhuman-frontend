a426c3f384172f1c4a086524653a18983256cb56
MBrian Website <muhammad.brian.27@gmail.com>
2023-02-23 09:59:33 +0700

    SET UP BASE PAGES

--- components/SideBar/index.vue
+++ components/SideBar/index.vue
@@ -28,14 +28,14 @@ import IconSignout from '~/assets/svgs/ic-signout.svg'
             <div class="flex flex-col gap-4">
                 <div class="text-sm text-grey">Daily Use</div>
                 <SidebarMenu url="/overview" :img="IconGrid" title="Overview" />
-                <SidebarMenu url="/employees" :img="IconUser" title="Employees" />
-                <SidebarMenu url="/teams" :img="IconBrief" title="My Teams" />
-                <SidebarMenu url="/roles" :img="IconFlag" title="Roles" />
+                <SidebarMenu url="/companies/1/employees" :img="IconUser" title="Employees" />
+                <SidebarMenu url="/companies/1/teams" :img="IconBrief" title="My Teams" />
+                <SidebarMenu url="/companies/1/roles" :img="IconFlag" title="Roles" />
             </div>
             <div class="flex flex-col gap-4">
                 <div class="text-sm text-grey">Others</div>
                 <SidebarMenu url="/insurance" :img="IconBox" title="Insurances" />
-                <SidebarMenu url="/company" :img="IconHome" title="Company" />
+                <SidebarMenu url="/companies" :img="IconHome" title="Company" />
                 <SidebarMenu url="/rewards" :img="IconGift" title="Rewards" />
                 <SidebarMenu url="/settings" :img="IconSetting" title="Settings" />
                 <SidebarMenu url="/logout" :img="IconSignout" title="LogOut" />
---
+++ pages/companies/index.vue
@@ -0,0 +1,36 @@
+<script setup>
+useHead({
+    title: 'PowerHuman HRIS - Select Company'
+})
+definePageMeta({
+    layout: 'full'
+})
+</script>
+<template>
+    <section class="py-[200px] flex flex-col items-center justify-center px-4">
+    <div class="text-[32px] font-semibold text-dark mb-4">Select Companies</div>
+    <div class="w-full card">
+            <div class="form-group">
+                <label for="" class="text-grey">Companies</label>
+
+                <!-- <p v-if="$fetchState.pending">Fetching companies...</p> -->
+                <select name="companies" id="" class="appearance-none input-field form-icon-chevron_down">
+                    <!-- <option :value="company.id" v-for="company in companies.data.result.data">
+                                            {{ company.name }}
+                                        </option> -->
+                    <option value="">blablabla</option>
+                    <option value="">blablabla</option>
+                    <option value="">blablabla</option>
+                    <option value="">blablabla</option>
+                </select>
+            </div>
+            <button type="button" class="w-full btn btn-primary mt-[14px]">
+                Continue
+            </button>
+            <div class="text-center">or</div>
+            <NuxtLink to="/companies/create" class="w-full border btn btn-white">
+                Create New Company
+            </NuxtLink>
+        </div>
+    </section>
+</template>
